47133904828722414df989f81b30d859a9527600
Leonardorossato <leonardorossato21@gmail.com>
2022-03-19 09:08:16 -0300

    correção

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,6 @@ const express = require('express')
 const UserController = require('../controllers/userController')
 const router = express.Router()
 
-router.post('/user', UserController)
+router.post('/user', UserController.createUser)
 
 module.exports = router
