a60fbc36a0d219447da61f152f4e99906c0a6da6
Leonardorossato <leonardorossato21@gmail.com>
2022-03-19 17:47:12 -0300

    autenticação de token do usuario e do admin

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,13 +2,28 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const process = process.env.process.privateKey
 
-const authentication = (req,res, next) => {
+const authenticationTokenUser = (req,res, next) => {
     const token = req.headers("x-auth-token")
     if(!token) res.status(400).json({message: "Access Denied. No token provided."})
 
     jwt.verify(token, privateKey, (err, validateToken)=>{
         if(err) res.status(400).json({message: "Invalid token"})
-        else requser = validateToken
+        else req.user = validateToken
         next()
     })
 }
+
+const authenticationTokenAdmin = (req,res, next) => {
+    const token = req.headers("x-auth-token")
+    if(!token) res.status(400).json({message: "Access Denied. No token provided."})
+
+    jwt.verify(token, privateKey, (err, validateToken)=>{
+        if(err) res.status(400).json({message: "Invalid token"})
+        else 
+            if(!validateToken) res.status(403).json({message: "You don't have access to this content"})
+        req.user = validateToken
+        next()
+    })
+}
+
+module.exports = {authenticationTokenUser, authenticationTokenAdmin}
