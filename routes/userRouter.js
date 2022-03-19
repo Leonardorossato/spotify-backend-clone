5ebabe026eb3b5e2fbc4161666c6a65992b3052b
Leonardorossato <leonardorossato21@gmail.com>
2022-03-19 14:51:51 -0300

    import da rota de autenticação

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const AuthController = require('../controllers/authController')
 const UserController = require('../controllers/userController')
 const router = express.Router()
 
+router.get('/', UserController.getAllUsers)
 router.post('/user', UserController.createUser)
+router.post('/login', AuthController.loginUserCredentials)
 
 module.exports = router
