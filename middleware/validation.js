b96601d4c7ded570829f65227735e49183428b07
Leonardorossato <leonardorossato21@gmail.com>
2022-03-19 14:51:10 -0300

    novo arquivo

---
+++ middleware/validation.js
@@ -0,0 +1,39 @@
+const passwordComplexity = require('joi-password-complexity')
+const Joi = require('joi')
+
+const validationUser = (user) =>{
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(100).required(),
+        email: Joi.string().email().required(),
+        password: passwordComplexity().required(),
+        month: Joi.string().required(),
+        date: Joi.string().required(),
+        year: Joi.string().required(),
+        gender: Joi.string().valid("male", "female", "non-binary").required(),
+    })
+    return schema.validate(user)
+}
+
+const validationSongs = (song) =>{
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        artist: Joi.string().required(),
+        song: Joi.string().required(),
+        img: Joi.string().required(),
+        duration: Joi.string().required(),
+    })
+    return schema.validate(song)
+}
+
+const validationPlaylist = (playlist) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        users: Joi.string().required(),
+        description: Joi.string().allow(""),
+        songs: Joi.array().required(),
+        img: Joi.string().allow(""),
+    })
+    return schema.validate(playlist)
+}
+
+module.exports = {validationUser, validationSongs, validationPlaylist}
