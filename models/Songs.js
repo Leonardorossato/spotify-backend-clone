24f15c97c358eba3c69a8c10dddeffeb2e46a120
Leonardorossato <leonardorossato21@gmail.com>
2022-03-20 11:03:37 -0300

    coreeção no model de song, erro era password no model

--- models/Songs.js
+++ models/Songs.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const songSchema = new mongoose.Schema({
     name : { type: String, required: true},
     artist : { type: String, required: true},
-    password : { type: String, required: true},
     song : { type: String, required: true},
     img : { type: String, required: true},
     duration : { type: String, required: true},
