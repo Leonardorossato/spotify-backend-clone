d4164b3be528403b7b9eb409c106d2996d304773
Leonardorossato <leonardorossato21@gmail.com>
2022-03-20 10:07:46 -0300

    rota do usuario e admin ok

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const router = express.Router()
 const AuthController = require('../controllers/authController')
 const UserController = require('../controllers/userController')
-const router = express.Router()
+const {authenticationTokenUser, authenticationTokenAdmin} = require('../middleware/authentication')
+const validObjectId = require('../middleware/validObjectId')
 
-router.get('/', UserController.getAllUsers)
+//rotas que o usuario tem permissão de usar
 router.post('/user', UserController.createUser)
 router.post('/login', AuthController.loginUserCredentials)
+router.put('/user/:id', authenticationTokenUser, validObjectId, UserController.updateUser)
 
+//rotas que o admin tem permissão para usar
+router.delete('/admin/:id', authenticationTokenAdmin, UserController.deleteUser)
+router.get('/admin',authenticationTokenAdmin ,UserController.getAllUsers)
+router.get('/admin/:id',authenticationTokenAdmin,validObjectId ,UserController.getUsersById)
 module.exports = router
