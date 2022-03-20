26f4ef3115816aca97b2b907836213a0bfd6beb8
Leonardorossato <leonardorossato21@gmail.com>
2022-03-20 10:52:07 -0300

    import da rota de musica para o server

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ require('dotenv').config()
 const port = process.env.port
 const mongoConnection = require('./connection/mongoConnection')
 const userRouter = require('./routes/userRouter')
+const songRouter = require('./routes/songRouter')
+
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.mongoConnection = mongoConnection
 
 app.use('/api/users', userRouter)
+app.use('/api/songs', songRouter)
 
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`)
