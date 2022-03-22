927efa5397ab64f9a98d5ca4ab5850e768654ac3
Leonardorossato <leonardorossato21@gmail.com>
2022-03-22 11:10:07 -0300

    correção

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,6 @@
 const { validationSongs } = require("../middleware/validation")
 const Songs = require("../models/Songs")
-const Users = require("../models/Users")
+const Users = require('../models/Users')
 
 class SongController{
     static createSong = async(req, res) => {
@@ -43,19 +43,23 @@ class SongController{
     }
 
     static LikedSongs = async(req, res) => {
+        let resMessage = ""
         const song = await Songs.findById(req.params.id)
         if(!song) res.status(200).json({message: "Song dosent not exists."})
 
         const user = await Users.findById(req.user._id)
         const index = user.likedSongs.indexOf(song._id)
+        console.log(index)
 
-        if(index == -1) {
+        if(index ===-1) {
             user.likedSongs.push(song._id) 
-            return res.status(200).json({message: "Added to your liked songs."})
+            resMessage ="Added to your liked songs."
         }else{
-            user.likedSongs.splice(index, 1)
-            return res.status(500).json({message: "Removed from your liked songs."})
+            user.likedSongs.splice(index,1)
+            resMessage: "Removed from your liked songs."
         }
+        await user.save()
+        return res.status(200).json({message: resMessage})
     }
 
     static getAllLikedSongs = async (req, res) => {
