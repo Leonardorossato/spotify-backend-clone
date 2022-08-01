e7a28821a642590441c5f6da5eeb76918248679d
tic02Costanox <99890694+tic02Costanox@users.noreply.github.com>
2022-08-01 17:13:46 -0300

    cadastro mudadao para a rota de auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,4 @@
-const bcrypt = require('bcrypt')
-const { validationUser } = require('../middleware/validation')
 const Users = require('../models/Users')
-require('dotenv').config()
-const secretSalt = process.env.secretSalt 
 
 class UserController{
 
@@ -21,37 +17,6 @@ class UserController{
             return res.status(200).json(user)
         } catch (error) {
             return res.status(500).json({error: error.message})
-        }
-    }
-    static createUser  = async(req, res) => {
-        const {error} = validationUser(req.body)
-        if (error) res.status(400).json({message: error.details[0].message})
-
-        const user = await Users.findOne({email: req.body.email})
-        if (user) {
-            return res.status(403).json({message: 'User with email already exists'})
-        }
-        const salt = await bcrypt.genSalt(Number(secretSalt))
-        const hashPassword = await bcrypt.hash(req.body.password, salt)
-        let newUser = await Users({
-            ...req.body,
-            password: hashPassword
-        }).save()
-
-        newUser.password = undefined
-        newUser.__v = undefined
-
-        return res.status(201).json(newUser)
-    }
-
-    static updateUser = async(req, res) => {
-        try {
-            const user = await Users.findByIdAndUpdate(req.params.id,{
-                $set: req.body
-            }, {new: true}).select("-password -__v")
-            return res.status(200).json({data: user})
-        } catch (error) {
-            return res.status(500).json({data: {error: error.message}})
         }
     }
 
