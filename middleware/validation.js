1d94858af97e081d097c1ce6ba78aab70c3d0335
tic02Costanox <99890694+tic02Costanox@users.noreply.github.com>
2022-08-01 17:13:56 -0300

    novas validations

--- middleware/validation.js
+++ middleware/validation.js
@@ -4,7 +4,7 @@ const Joi = require('joi')
 const validationUser = (user) =>{
     const schema = Joi.object({
         name: Joi.string().min(5).max(100).required(),
-        email: Joi.string().email().required(),
+        email: Joi.string().email().min(3).max(200).required(),
         password: passwordComplexity().required(),
         month: Joi.string().required(),
         date: Joi.string().required(),
