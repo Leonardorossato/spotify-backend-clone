f973b0223ba1f6be5df0e6af9a904da87db157ff
tic02Costanox <99890694+tic02Costanox@users.noreply.github.com>
2022-08-03 10:02:20 -0300

    limpeza

--- controllers/songController.js
+++ controllers/songController.js
@@ -43,23 +43,27 @@ class SongController{
     }
 
     static LikedSongs = async(req, res) => {
-        let resMessage = ""
-        const song = await Songs.findById(req.params.id)
-        if(!song) res.status(200).json({message: "Song dosent not exists."})
+        try {
+            let resMessage = ""
+            const song = await Songs.findById(req.params.id)
+            if(!song) res.status(400).json({message: "Song dosent not exists."})
 
-        const user = await Users.findById(req.user._id)
-        const index = user.likedSongs.indexOf(song._id)
-        console.log(index)
+            const user = await Users.findById(req.user._id)
+            const index = user.likedSongs.indexOf(song._id)
+            console.log(index)
 
-        if(index ===-1) {
-            user.likedSongs.push(song._id) 
-            resMessage ="Added to your liked songs."
-        }else{
-            user.likedSongs.splice(index,1)
-            resMessage: "Removed from your liked songs."
+            if(index === -1) {
+                user.likedSongs.push(song._id) 
+                resMessage ="Added to your liked songs."
+            }else{
+                user.likedSongs.splice(index,1)
+                resMessage: "Removed from your liked songs."
+            }
+            await user.save()
+            return res.status(200).json({message: resMessage})
+        } catch (error) {
+            return res.status(500).json({message: error.message})
         }
-        await user.save()
-        return res.status(200).json({message: resMessage})
     }
 
     static getAllLikedSongs = async (req, res) => {
