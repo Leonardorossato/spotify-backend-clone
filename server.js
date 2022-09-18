1aeac34232c02e38f30a220c2dcb96ae62f29243
Leonardorossato <leonardorossato21@gmail.com>
2022-09-18 09:26:12 -0300

    comentario da rota que n esta sendo utilizada

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const PORT = process.env.PORT
+const swaggerUi = require('swagger-ui-express')
 const mongoConnection = require('./connection/mongoConnection')
 const userRouter = require('./routes/userRouter')
 const songRouter = require('./routes/songRouter')
-const playlistRouter = require('./routes/playlistRouter')
+//const playlistRouter = require('./routes/playlistRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -14,7 +15,8 @@ app.mongoConnection = mongoConnection
 
 app.use('/api/users', userRouter)
 app.use('/api/songs', songRouter)
-app.use('/api/playlists', playlistRouter)
+//app.use('/api/playlists', playlistRouter)
+app.use(swaggerUi.serve)
 
 app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`)
