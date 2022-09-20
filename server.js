571a492fa49383168927a243e90376171f12a9f4
Leonardorossato <leonardorossato@gmail.com>
2022-09-20 16:08:43 -0300

    rota de playlist ok

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const PORT = process.env.PORT
-const swaggerUi = require('swagger-ui-express')
 const mongoConnection = require('./connection/mongoConnection')
 const userRouter = require('./routes/userRouter')
 const songRouter = require('./routes/songRouter')
-//const playlistRouter = require('./routes/playlistRouter')
+const playlistRouter = require('./routes/playlistRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -15,8 +14,8 @@ app.mongoConnection = mongoConnection
 
 app.use('/api/users', userRouter)
 app.use('/api/songs', songRouter)
-//app.use('/api/playlists', playlistRouter)
-app.use(swaggerUi.serve)
+app.use('/api/playlists', playlistRouter)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`)
