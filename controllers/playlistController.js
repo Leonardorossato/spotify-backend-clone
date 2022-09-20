1a2de95b12d838bc75a65913aba0c94d7b6a47b4
Leonardorossato <leonardorossato@gmail.com>
2022-09-20 16:09:08 -0300

    afzendo o metodo de controle ddas playlists

---
+++ controllers/playlistController.js
@@ -0,0 +1,63 @@
+const Joi = require("joi");
+const { validationPlaylist } = require("../middleware/validation");
+const Playlists = require("../models/Playlist");
+const Users = require("../models/Users");
+
+class PlaylistController{
+    static createPlaylist = async(req, res) => {
+
+        try {
+            const { error } = validationPlaylist(req.body);
+            if (error) return res.status(400).send({message: error.details[0].message });
+
+            const user = await Users.findById(req.user._id).lean();
+            const playList = await Playlists({...req.body, user: user['_id']._id}).save()
+            user.playlist.push(playList._id)
+
+            await playList.save();
+            return res.status(201).json(playList);
+        } catch (error) {
+            return res.status(404).json({error: error.message})
+        }
+    }
+    
+    static getAllPlaylists = async(req, res)=>{
+        try {
+           
+            const playlists = await Playlists.find();
+            return res.status(200).json({playlists});
+        } catch (error) {
+            return res.status(500).json({error: error.message})
+        }
+    }
+
+    static editPlaylistById = async(req, res)=>{
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            desc: Joi.string().allow(""),
+            img: Joi.string().allow(""),
+        });
+        try {
+            const { error } = schema.validate(req.body);
+            if (error) return res.status(400).send({ message: error.details[0].message });
+
+            const playlist = await Playlists.findById(req.params.id);
+            if (!playlist) return res.status(404).send({ message: "Playlist not found" });
+
+            const user = await Users.findById(req.user._id).lean();
+            if (!user._id.equals(playlist.user))
+                return res.status(403).send({ message: "User don't have access to edit!" });
+
+            playlist.name = req.body.name;
+            playlist.desc = req.body.desc;
+            playlist.img = req.body.img;
+            await playlist.save();
+
+            return res.status(200).send({ message: "Playlist updatedq successfully" });
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = PlaylistController
