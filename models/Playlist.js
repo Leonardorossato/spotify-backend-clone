1457fa8a9c0721ae740a8c7b382acbb00d6e0c3d
Leonardorossato <leonardorossato@gmail.com>
2022-09-20 16:10:15 -0300

    variavel do objeto Id

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const ObjectId = mongoose.Types.ObjectId;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const playlistSchema = new mongoose.Schema({
     name : { type: String, required: true},
-    users: {type: ObjectId, ref: 'users', required: true},
+    user: {type: ObjectId, ref: 'users', required: true},
     description: { type: String , required: false},
-    songs: { type: Array, default: []},
+    songs: { type: Array, default: [], required: false},
     img: { type: String , required: false}
 })
 
