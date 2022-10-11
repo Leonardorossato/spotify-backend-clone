af4dc12d5ccea68a7bc2e835bc5bb91fc3e16ffe
Leonardo Adami Rossato Rodrigues <34960160+Leonardorossato@users.noreply.github.com>
2022-10-11 10:59:12 -0300

    todos podem criar as musicas agora

--- routes/songRouter.js
+++ routes/songRouter.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/all', SongController.getAllSong)
 router.get('/search',authenticationTokenUser ,SongController.searchPartitionSong)
-router.post('/admin/create', authenticationTokenAdmin ,SongController.createSong)
+router.post('/admin/create', authenticationTokenAdmin, authenticationTokenUser ,SongController.createSong)
 router.put('/admin/song/:id', authenticationTokenAdmin, validObjectId, SongController.updateSong)
 router.put('/like/song/:id', validObjectId,authenticationTokenUser,SongController.LikedSongs)
 router.delete('/admin/song/:id', authenticationTokenAdmin, validObjectId, SongController.deleteSong)
